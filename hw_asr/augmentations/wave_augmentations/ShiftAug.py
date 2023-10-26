d389427916dd9a004243999e81b911f3121e3058
Oleg <43697696+algorithm-pirogok@users.noreply.github.com>
2023-10-27 01:59:43 +0300

    Update ShiftAug.py

--- hw_asr/augmentations/wave_augmentations/ShiftAug.py
+++ hw_asr/augmentations/wave_augmentations/ShiftAug.py
@@ -1,5 +1,3 @@
-from random import random
-
 import torch_audiomentations
 from torch import Tensor
 
@@ -8,7 +6,7 @@ from hw_asr.augmentations.base import AugmentationBase
 
 class ShiftAugmentation(AugmentationBase):
     def __init__(self, *args, **kwargs):
-        self._aug = torch_audiomentations.PitchShift(*args, **kwargs)  # 4 augmentation
+        self._aug = torch_audiomentations.PitchShift(sample_rate=16000, *args, **kwargs)  # 4 augmentation
 
     def __call__(self, data: Tensor):
         x = data.unsqueeze(1)
